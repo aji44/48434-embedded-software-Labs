9b9c637f37d8b214d803e38243e86ceb2da6d28d
Corey Stidston <coreystidston@Coreys-MacBook-Pro.local>
2017-04-14 23:37:38 +1000

    Added half-word and word methods (Flash_AllocateVar2 & Flash_AllocateVar4 respectively). Added more test scenarios

--- Test_Programs/Lab2_Flash.c
+++ Test_Programs/Lab2_Flash.c
@@ -6,106 +6,201 @@ int phrase = 0xFF; /* represents the 8bytes of space in flash memory - Find bett
 
 int Flash_AllocateVar1(); /* 8bytes on any address */
 int Flash_AllocateVar2(); /* 16bytes only on even address */
+int Flash_AllocateVar4(); /* 16bytes only on address divisible by 4 i.e. 80000 or 80004 */
 
 void test_1();
 void test_2();
 void test_3();
+void test_4();
+void test_5();
 
 int main(void)
 {
-	test_2();
+	test_5();
 
 	return 0;
 }
 
 int Flash_AllocateVar1()
 {
+	/*
+	Mask values: 
+	0080 (initial)
+	0040
+	0020
+	0010
+	0008
+	0004
+	0002
+	0001 (final)
+	0000 (invalid)
+	*/
 	int mask = 0x80; 
+	printf("Initial Mask - %04x\n", mask);
 	int pos;
 	int variable;
 
 	for(pos = 0x00; pos < 0x08; pos += 0x01) {
-		if(phrase & mask) {
+		if(mask == (phrase & mask)) {
 			variable = FLASH_DATA_START + pos; 
-			printf("Variable Address = %d\n", variable);
+			/* printf("Variable Address = %d\n", variable); */
 			phrase = (phrase ^ mask);
-			return 1;
+			return variable;
 		}
 		mask = mask >> 1;
-		printf("Mask - %d\n", mask);
+		printf("Mask - %04x\n", mask);
 	}
-	return 0;
+	return -1;
 }
 
 int Flash_AllocateVar2()
 {
 	/*
 	Mask values: 
-	192
-	48
-	12
-	3
+	00c0
+	0030
+	000c
+	0003 (final)
+	0000 (invalid)
 	*/
 
-	int mask1 = 0x80; /* 	10000000 */
-	int mask2 = 0x40;  /* 	01000000 */
+	int mask = 0xC0;
+	printf("Initial Mask - %04x\n", mask);
 	int pos;
 	int variable;
 
 	for(pos = 0x00; pos < 0x08; pos += 0x02) {
-		/* if(mask == (phrase & mask)) */
-		if((phrase & mask1) & mask2) {
+		if(mask == (phrase & mask)) {
 			variable = FLASH_DATA_START + pos; 
-			printf("Variable Address = %d\n", variable);
-			phrase = (phrase ^ (mask1 | mask2));
-
-			return 1;
+			/* printf("Variable Address = %d\n", variable); */
+			phrase = (phrase ^ mask);
+			return variable;
 		}
 
 		/*mask = mask >> 2;  I.E 11000000 -> 00110000*/
 
-		mask1 = mask1 >> 2; 
-		mask2 = mask2 >> 2;
-		printf("Mask1 - %d\n", mask1);
-		printf("Mask2 - %d\n", mask2);
+		mask = mask >> 2; 
+		printf("Mask - %04x\n", mask);
 	}
-	return 0;
+	return -1;
 }
 
+int Flash_AllocateVar4()
+{
+	/*
+	Mask values: 
+	00f0 (initial)
+	000f (final)
+	0000 (invalid)
+	*/
 
+	int mask = 0xF0; /* 11110000 */
+	printf("Initial Mask - %04x\n", mask);
+	int pos;
+	int variable;
+
+	for(pos = 0x00; pos < 0x08; pos += 0x04) {
+		if(mask == (phrase & mask)) {
+			variable = FLASH_DATA_START + pos; 
+			/* printf("Variable Address = %d\n", variable); */
+			phrase = (phrase ^ mask);
+			return variable;
+		}
+
+		mask = mask >> 4; /* 11110000 -> 00001111*/
+		printf("Mask - %04x\n", mask);
+	}
+	return -1;
+}
 
 /*****************************************TESTS*****************************************/
 
 
 void test_1()
 {
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*1 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*2 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*3 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*4 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*5 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*6 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*7 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*8 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*9 should fail, no more room  */
-	printf("Allocated? = %d\n",Flash_AllocateVar1());  /*10 should fail, no more room */
+	int out;
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*1 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*2 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*3 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*4 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*5 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*6 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*7 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*8 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*9 should fail, no more room  */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out);  /*10 should fail, no more room */
 }
 
 void test_2()
 {
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*1 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*2 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*3 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*4 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*5 should fail, no more room  */
+	int out;
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*1 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*2 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*3 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*4 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*5 should fail, no more room  */
 }
 
 void test_3()
 {
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*1 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar1()); /*2 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*3 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*4 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*4 should succeed */
-	printf("Allocated? = %d\n",Flash_AllocateVar2()); /*5 should fail, no more room  */
+	int out;
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*1 should succeed */
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out); /*2 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*3 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*4 should succeed */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*4 should fail, no more room */
+	out = Flash_AllocateVar2();
+	printf("Allocated? (%04x,  %04x) \n", out, (1+out)); /*5 should fail, no more room  */
+}
+
+void test_4()
+{
+	int out;
+	out = Flash_AllocateVar4();
+	printf("Allocated? (%04x - %04x) \n", out, (3+out)); /*1 should succeed*/
+	out = Flash_AllocateVar4();
+	printf("Allocated? (%04x - %04x) \n", out, (3+out)); /*2 should succeed*/
+	out = Flash_AllocateVar4();
+	printf("Allocated? (%04x - %04x) \n", out, (3+out)); /*3 should fail, no more room*/
+}
+
+void test_5()
+{
+	int out;
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out); /*1 should succeed*/
+	out = Flash_AllocateVar4();
+	printf("Allocated? (%04x - %04x) \n", out, (3+out)); /*2 should succeed*/
+	out = Flash_AllocateVar4();
+	printf("Allocated? (%04x - %04x) \n", out, (3+out)); /*3 should fail no more room*/
+	out = Flash_AllocateVar4();
+	printf("Allocated? (%04x - %04x) \n", out, (3+out)); /*4 should fail, no more room*/
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out); /*5 should succeed*/
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out); /*6 should succeed*/
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out); /*7 should succeed*/
+	out = Flash_AllocateVar1();
+	printf("Allocated? (%04x) \n", out); /*8 should fail, no more room*/
 }
