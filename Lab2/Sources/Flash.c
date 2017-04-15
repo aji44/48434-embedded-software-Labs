890467eb8ada2928fa74d0a7968804f21a128a82
Corey Stidston <coreystidston@Coreys-MacBook-Pro.local>
2017-04-15 14:46:19 +1000

    Work on Flash Write, Erase

--- Lab2/Sources/Flash.c
+++ Lab2/Sources/Flash.c
@@ -5,7 +5,15 @@
  *      Author: 98119910
  */
 
+#include "types.h"
 #include "Flash.h"
+#include "MK70F12.h"
+
+static bool LaunchCommand(TFCCOB* commonCommandObject);
+static bool WritePhrase(const uint32_t address, const uint64union_t phrase);
+static bool ReadPhrase(uint64_t * const phrase);
+static void WaitCCIF(void);
+static void SetCCIF(void);
 
 uint8_t phrase = 0xFF; //11111111
 
@@ -35,7 +43,6 @@ bool Flash_AllocateVar(volatile void** variable, const uint8_t size)
 {
 	int mask;
 	int addressPos;
-	int retVal;
 
 	switch(size)
 	{
@@ -70,7 +77,31 @@ bool Flash_AllocateVar(volatile void** variable, const uint8_t size)
  */
 bool Flash_Write32(volatile uint32_t* const address, const uint32_t data)
 {
+	uint8_t index = address - FLASH_DATA_START;
+	uint64_t mask = 0xFFFFFFFF00000000U
+	uint64_t newPhrase = 0x0000000000000000U
 
+	uint64_t currentPhrase;
+	ReadPhrase(&currentPhrase);
+
+	uint16_t tempPhrase[2];
+
+	uint8_t i;
+	for(i = 0; i < 8; i += 4) {
+		if(i = index) {
+			tempPhrase[i] = data;
+		} else {
+			tempPhrase[i] = ((currentPhrase & data) >> (64-(32*(i+1))));
+		}
+		mask = mask >> 32;
+	}
+
+	for(i = 0; i < 8; i += 4) {
+		newPhrase = newPhrase | tempPhrase[i];
+	}
+	
+	WritePhrase(newPhrase);
+	return true;
 }
 
 /*! @brief Writes a 16-bit number to Flash.
@@ -82,7 +113,31 @@ bool Flash_Write32(volatile uint32_t* const address, const uint32_t data)
  */
 bool Flash_Write16(volatile uint16_t* const address, const uint16_t data)
 {
+	uint8_t index = address - FLASH_DATA_START;
+	uint64_t mask = 0xFFFF000000000000U
+	uint64_t newPhrase = 0x0000000000000000U
 
+	uint64_t currentPhrase;
+	ReadPhrase(&currentPhrase);
+
+	uint16_t tempPhrase[4];
+
+	uint8_t i;
+	for(i = 0; i < 8; i += 2) {
+		if(i = index) {
+			tempPhrase[i] = data;
+		} else {
+			tempPhrase[i] = ((currentPhrase & data) >> (64-(16*(i+1))));
+		}
+		mask = mask >> 16;
+	}
+
+	for(i = 0; i < 8; i += 2) {
+		newPhrase = newPhrase | tempPhrase[i];
+	}
+	
+	WritePhrase(newPhrase);
+	return true;
 }
 
 /*! @brief Writes an 8-bit number to Flash.
@@ -94,7 +149,93 @@ bool Flash_Write16(volatile uint16_t* const address, const uint16_t data)
  */
 bool Flash_Write8(volatile uint8_t* const address, const uint8_t data)
 {
+	uint8_t index = address - FLASH_DATA_START;
+	uint64_t mask = 0xFF00000000000000U
+	uint64_t newPhrase = 0x0000000000000000U
 
+	uint64_t currentPhrase;
+	ReadPhrase(&currentPhrase);
+
+	uint8_t tempPhrase[8];
+
+	uint8_t i;
+	for(i = 0; i < 8; i++) {
+		if(i = index) {
+			tempPhrase[i] = data;
+		} else {
+			tempPhrase[i] = ((currentPhrase & data) >> (64-(8*(i+1))));
+		}
+		mask = mask >> 8;
+	}
+
+	for(i = 0; i < 8; i++) {
+		newPhrase = newPhrase | tempPhrase[i];
+	}
+	
+	WritePhrase(newPhrase);
+	return true;
+}
+
+//P 789 and P806
+bool WritePhrase(const uint64union_t phrase) //const uint32_t address, 
+{
+	uint32_8union_t flashStart;
+
+	uint32union_t word_Hi;
+	uint32union_t word_Lo;
+
+	uint16union_t halfword_1; //16 bytes
+	uint16union_t halfword_2;
+	uint16union_t halfword_3;
+	uint16union_t halfword_4;
+
+	WaitCCIFReady();
+
+	if(!Flash_Erase())
+	{
+		return false;
+	}
+
+	flashStart.l = FLASH_DATA_START;
+	FTFE_FCCOB0 = FLASH_CMD_PGM8; // defines the FTFE command to write
+	FTFE_FCCOB1 = flashStart.s.b; // sets flash address[23:16] to 128
+	FTFE_FCCOB2 = flashStart.s.c; // sets flash address[15:8] to 0
+	FTFE_FCCOB3 = (flashStart.s.d & 0xF8);
+
+	word_Hi = phrase.Hi;
+	word_Lo = phrase.Lo;
+
+	halfword_1 = word_Hi.Hi
+	halfword_2 = word_Hi.Lo
+	halfword_3 = word_Lo.Hi
+	halfword_4 = word_Lo.Lo
+
+	//Big Endian Sorted- is this Correct?
+	FTFE_FCCOB4 = halfword_3.Hi
+	FTFE_FCCOB5 = halfword_3.Lo
+	FTFE_FCCOB6 = halfword_4.Hi 
+	FTFE_FCCOB7 = halfword_4.Lo 
+	FTFE_FCCOB8 = halfword_2.Hi
+	FTFE_FCCOB9 = halfword_2.Lo
+	FTFE_FCCOBA = halfword_1.Hi
+	FTFE_FCCOBB = halfword_1.Lo
+
+	SetCCIF(); //Initiates the command
+
+	return true;
+}
+
+/*! @brief Reads the phrase starting from FLASH_DATA_START
+ *	
+ *	@param returns pointer to phrase
+ *  @return bool - TRUE if the the phrase was read
+ *  @note Assumes Flash has been initialized.
+ */
+bool ReadPhrase(uint64_t * const phrase)
+{
+	WaitCCIFReady();
+	*phrase = _FP(FLASH_DATA_START);
+	return true;
 }
 
 /*! @brief Erases the entire Flash sector.
@@ -104,69 +245,17 @@ bool Flash_Write8(volatile uint8_t* const address, const uint8_t data)
  */
 bool Flash_Erase(void)
 {
-
+	WaitCCIFReady();
+	FTFE_FCCOB0 = 0x09; //Command to erase flash sector
+	return true; //Later on, need to check error flags
 }
 
+void WaitCCIF(void)
+{
+	while (!(FTFE_FSTAT & FTFE_FSTAT_CCIF_MASK));
+}
 
-// switch(size)
-//   {
-// 	case 1:
-// 	  uint8_t mask = 0x80; //10000000
-// 	  uint8_t pos;
-
-// 	  for(pos = 0x00; pos < 0x08; pos += 0x01) {
-// 		  if(phrase & mask) {
-// 			  variable = FLASH_DATA_START + pos; //address of unallocated flash memory
-// 			  phrase = (phrase ^ mask); //update the phrase
-// 			  return true;
-// 		  }
-// 		  mask = mask >> 1;
-// 	  }
-// 	  return false;
-// 	  break;
-// 	case 2:
-// 		//NEEDS FIXING
-// 	  uint8_t mask = 0xC0; //11000000
-
-// 	  uint8_t pos;
-// 	  for(pos = 0x00; pos < 0x08; pos += 0x02) {
-// 		  if(phrase & mask) {
-// 			  variable = FLASH_DATA_START + pos; //address of unallocated flash memory
-// 			  phrase = ~(phrase & mask); //update the phrase
-// 			  return true;
-// 		  }
-// 		  mask = mask >> 2;
-// 	  }
-// 	  return false;
-// 	  break;
-// 	case 4:
-
-// 	  break;
-// 	case 8:
-
-// 	  break;
-//   }
-
-
-//  if(phrase & mask){
-//	  variable = FLASH_DATA_START;
-//
-//	  return true;
-//  }
-//  mask = mask >> 1;
-//  if(phrase & mask){
-//	  variable = FLASH_DATA_START + 1;
-//	  return true;
-//  }
-//  mask = mask >> 1;
-//  if(phrase & mask){
-//	  variable = FLASH_DATA_START + 2;
-//	  return true;
-//  }
-//
-//	  for(mask = 0x80; mask > 0x1; mask >> 1) {
-//
-//	  }
-
-
-
+void SetCCIF(void)
+{
+	FTFE_FSTAT |= FTFE_FSTAT_CCIF_MASK;
+}
