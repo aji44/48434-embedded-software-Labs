4bf40157944f37d02129af3abde539e3ec46509a
corey-stidston <coreystidston@Coreys-MacBook-Pro.local>
2017-04-17 12:21:42 +1000

    Tidying Flash.c

--- Lab2/Sources/Flash.c
+++ Lab2/Sources/Flash.c
@@ -88,34 +88,6 @@ bool Flash_Write32(volatile uint32_t* const address, const uint32_t data)
 	psuedoArray[index] = data;
 	WritePhrase(newPhrase);
 	return true;
-
-	/*
-	uint8_t index = *address - FLASH_START;
-	uint64_t mask = 0xFFFFFFFF00000000U
-	uint64_t newPhrase = 0x0000000000000000U
-
-	uint64_t currentPhrase;
-	ReadPhrase(&currentPhrase);
-
-	uint16_t tempPhrase[2];
-
-	uint8_t i;
-	for(i = 0; i < 8; i += 4) {
-		if(i = index) {
-			tempPhrase[i] = data;
-		} else {
-			tempPhrase[i] = ((currentPhrase & data) >> (64-(32*(i+1))));
-		}
-		mask = mask >> 32;
-	}
-
-	for(i = 0; i < 8; i += 4) {
-		newPhrase = newPhrase | tempPhrase[i];
-	}
-	
-	WritePhrase(newPhrase);
-	return true;
-	*/
 }
 
 /*! @brief Writes a 16-bit number to Flash.
@@ -138,32 +110,6 @@ bool Flash_Write16(volatile uint16_t* const address, const uint16_t data)
 	psuedoArray[index] = data;
 	WritePhrase(newPhrase);
 	return true;
-
-	// uint8_t index = *address - FLASH_START;
-	// uint64_t mask = 0xFFFF000000000000U
-	// uint64_t newPhrase = 0x0000000000000000U
-
-	// uint64_t currentPhrase;
-	// ReadPhrase(&currentPhrase);
-
-	// uint16_t tempPhrase[4];
-
-	// uint8_t i;
-	// for(i = 0; i < 8; i += 2) {
-	// 	if(i = index) {
-	// 		tempPhrase[i] = data;
-	// 	} else {
-	// 		tempPhrase[i] = ((currentPhrase & data) >> (64-(16*(i+1))));
-	// 	}
-	// 	mask = mask >> 16;
-	// }
-
-	// for(i = 0; i < 8; i += 2) {
-	// 	newPhrase = newPhrase | tempPhrase[i];
-	// }
-	
-	// WritePhrase(newPhrase);
-	// return true;
 }
 
 /*! @brief Writes an 8-bit number to Flash.
@@ -186,33 +132,6 @@ bool Flash_Write8(volatile uint8_t* const address, const uint8_t data)
 	psuedoArray[index] = data;
 	WritePhrase(newPhrase);
 	return true;
-	
-	/*
-	uint8_t index = *address - FLASH_START;
-	uint64_t mask = 0xFF00000000000000U
-	uint64_t newPhrase = 0x0000000000000000U
-
-	uint64_t currentPhrase;
-	ReadPhrase(&currentPhrase);
-
-	uint8_t tempPhrase[8];
-
-	uint8_t i;
-	for(i = 0; i < 8; i++) {
-		if(i = index) {
-			tempPhrase[i] = data;
-		} else {
-			tempPhrase[i] = ((currentPhrase & data) >> (64-(8*(i+1))));
-		}
-		mask = mask >> 8;
-	}
-
-	for(i = 0; i < 8; i++) {
-		newPhrase = newPhrase | tempPhrase[i];
-	}
-	
-	WritePhrase(newPhrase);
-	return true;*/
 }
 
 //P 789 and P806
@@ -275,10 +194,9 @@ bool ReadPhrase(uint64_t * const phrase)
  */
 bool Flash_Erase(void)
 {
-<<<<<<< HEAD
 	WaitCCIF();
 	FTFE_FCCOB0 = 0x09; //Command to erase flash sector
-=======
+
 	//WaitCCIFReady();
 	//FTFE_FCCOB0 = 0x09; //Command to erase flash sector
 	//return true; //Later on, need to check error flags
@@ -304,7 +222,6 @@ bool Flash_Erase(void)
   // // Only do this if you want the allocation to clear too.
   // //	memset(allocationMap, 0, FLASH_DATA_SIZE);
   // return HandleErrorRegisters(); pg 783/784 K70 manual
->>>>>>> 82c13f6b3e746ab1652d5f67a759bbe8e5df9c3b
 	return true; //Later on, need to check error flags
 }
 
@@ -318,66 +235,12 @@ void WaitCCIF(void)
 
 void SetCCIF(void)
 {
-<<<<<<< HEAD
 	FTFE_FSTAT |= FTFE_FSTAT_CCIF_MASK;
 }
 
 
 
-/*
-bool WritePhrase(const uint64union_t phrase) //const uint32_t address, 
-{
-	uint32_8union_t flashStart; //notes: no uint32_8union_t in our types.h
 
-	uint32union_t word_Hi;
-	uint32union_t word_Lo;
-
-	uint16union_t halfword_1; //16 bytes
-	uint16union_t halfword_2;
-	uint16union_t halfword_3;
-	uint16union_t halfword_4;
-
-	WaitCCIF();
-
-	if(!Flash_Erase())
-	{
-		return false;
-	}
-
-	flashStart.l = FLASH_START;
-	FTFE_FCCOB0 = FLASH_CMD_PGM8; // defines the FTFE command to write
-	FTFE_FCCOB1 = flashStart.s.b; // sets flash address[23:16] to 128
-	FTFE_FCCOB2 = flashStart.s.c; // sets flash address[15:8] to 0
-	FTFE_FCCOB3 = (flashStart.s.d & 0xF8);
-
-	//need to fi this up: no uint64uniont etc..
-	
-	word_Hi = phrase.Hi;
-	word_Lo = phrase.Lo;
-
-	halfword_1 = word_Hi.Hi
-	halfword_2 = word_Hi.Lo
-	halfword_3 = word_Lo.Hi
-	halfword_4 = word_Lo.Lo
-
-	//Big Endian Sorted- is this Correct?
-	FTFE_FCCOB4 = halfword_3.Hi
-	FTFE_FCCOB5 = halfword_3.Lo
-	FTFE_FCCOB6 = halfword_4.Hi 
-	FTFE_FCCOB7 = halfword_4.Lo 
-	FTFE_FCCOB8 = halfword_2.Hi
-	FTFE_FCCOB9 = halfword_2.Lo
-	FTFE_FCCOBA = halfword_1.Hi
-	FTFE_FCCOBB = halfword_1.Lo
-
-	SetCCIF(); //Initiates the command
-	WaitCCIF();
-	return true;
-}
-
-
-*/
-=======
 	//All required FCCOBx registers are written, so launch the command
   // This line is occurred ACCERR.
   //  FTFE_FSTAT = FTFE_FSTAT_CCIF_MASK;
@@ -387,8 +250,4 @@ bool WritePhrase(const uint64union_t phrase) //const uint32_t address,
   // completed. If CCIF is zero, the previous command execution is still active, a new
   // command write sequence cannot be started, and all writes to the FCCOB registers are
   // ignored.
-    FTFE_FSTAT |= FTFE_FSTAT_CCIF_MASK;
-	//wait for flash module start up
-    WaitCCIF();
-}
->>>>>>> 82c13f6b3e746ab1652d5f67a759bbe8e5df9c3b
+
