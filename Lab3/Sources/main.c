672dfbc69870cbaa251f7df25b3636440aee95b2
corey-stidston <Corey.Stidston@student.uts.edu.au>
2017-04-24 16:00:46 +1000

    Minor changes to main.c

--- Lab3/Sources/main.c
+++ Lab3/Sources/main.c
@@ -49,14 +49,14 @@ void TowerInit(void)
 	bool flashStatus  = Flash_Init();
 	bool ledStatus 	  = LEDs_Init();
 	if(!(packetStatus && flashStatus && ledStatus))
-	{
-		LEDs_On(LED_ORANGE);	//Tower was initialized correctly
+		{
+			LEDs_On(LED_ORANGE);	//Tower was initialized correctly
 
-		Packet_Put(TOWER_STARTUP_COMM, TOWER_STARTUP_PAR1, TOWER_STARTUP_PAR2, TOWER_STARTUP_PAR3);
-		Packet_Put(TOWER_NUMBER_COMM, TOWER_NUMBER_PAR1, TowerNumber->s.Lo, TowerNumber->s.Hi);
-		Packet_Put(TOWER_VERSION_COMM, TOWER_VERSION_V, TOWER_VERSION_MAJ, TOWER_VERSION_MIN);
-		Packet_Put(TOWER_MODE_COMM, TOWER_MODE_PAR1, TowerMode->s.Lo, TowerMode->s.Hi);
-	}
+			Packet_Put(TOWER_STARTUP_COMM, TOWER_STARTUP_PAR1, TOWER_STARTUP_PAR2, TOWER_STARTUP_PAR3);
+			Packet_Put(TOWER_NUMBER_COMM, TOWER_NUMBER_PAR1, TowerNumber->s.Lo, TowerNumber->s.Hi);
+			Packet_Put(TOWER_VERSION_COMM, TOWER_VERSION_V, TOWER_VERSION_MAJ, TOWER_VERSION_MIN);
+			Packet_Put(TOWER_MODE_COMM, TOWER_MODE_PAR1, TowerMode->s.Lo, TowerMode->s.Hi);
+		}
 }
 
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
@@ -70,19 +70,19 @@ int main(void)
 	/*** End of Processor Expert internal initialization.                    ***/
 
 	/* Write your code here */
-	__DI(); 		//Disable interrupts before peripheral module initialization
+	//__DI(); 		//Disable interrupts before peripheral module initialization
 	TowerInit();	//Initialize tower peripheral modules
-	__EI(); 		//Enable interrups
+	//__EI(); 		//Enable interrupts
 
 	for (;;)
-	{
-		if (Packet_Get())	//Check if there is a packet in the retrieved data
 		{
-			Packet_Handle();
-			LEDs_Toggle(LED_BLUE);
+			if (Packet_Get())	//Check if there is a packet in the retrieved data
+				{
+					Packet_Handle();
+					LEDs_Toggle(LED_BLUE);
+				}
+			//UART_Poll();	//Checks whether the RDRF or TDRE flags are set and retrieves/transmits data
 		}
-		UART_Poll();	//Checks whether the RDRF or TDRE flags are set and retrieves/transmits data
-	}
 
 	/*** Don't write any code pass this line, or it will be deleted during code generation. ***/
 	/*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
