0707e0793f65599b57a512b5843d4cc6cb2facc6
corey-stidston <Corey.Stidston@student.uts.edu.au>
2017-04-24 13:11:29 +1000

    Moved peripheral module initialisations to function called TowerInit()

--- Lab3/Sources/main.c
+++ Lab3/Sources/main.c
@@ -43,6 +43,22 @@
 const static uint32_t BAUD_RATE = 115200;
 const static uint32_t MODULE_CLOCK = CPU_BUS_CLK_HZ;
 
+void TowerInit(void)
+{
+	bool packetStatus = Packet_Init(BAUD_RATE, MODULE_CLOCK);
+	bool flashStatus  = Flash_Init();
+	bool ledStatus 	  = LEDs_Init();
+	if(!(packetStatus && flashStatus && ledStatus))
+	{
+		LEDs_On(LED_ORANGE);	//Tower was initialized correctly
+
+		Packet_Put(TOWER_STARTUP_COMM, TOWER_STARTUP_PAR1, TOWER_STARTUP_PAR2, TOWER_STARTUP_PAR3);
+		Packet_Put(TOWER_NUMBER_COMM, TOWER_NUMBER_PAR1, TowerNumber->s.Lo, TowerNumber->s.Hi);
+		Packet_Put(TOWER_VERSION_COMM, TOWER_VERSION_V, TOWER_VERSION_MAJ, TOWER_VERSION_MIN);
+		Packet_Put(TOWER_MODE_COMM, TOWER_MODE_PAR1, TowerMode->s.Lo, TowerMode->s.Hi);
+	}
+}
+
 /*lint -save  -e970 Disable MISRA rule (6.3) checking. */
 int main(void)
 /*lint -restore Enable MISRA rule (6.3) checking. */
@@ -54,31 +70,18 @@ int main(void)
 	/*** End of Processor Expert internal initialization.                    ***/
 
 	/* Write your code here */
-	bool packetStatus = Packet_Init(BAUD_RATE, MODULE_CLOCK);
-	bool flashStatus  = Flash_Init();
-	bool ledStatus 	  = LEDs_Init();
-	if(!(packetStatus && flashStatus && ledStatus))
-	{
-		const TLED colour = LED_ORANGE;
-		LEDs_On(colour);
-
-		Packet_Put(TOWER_STARTUP_COMM, TOWER_STARTUP_PAR1, TOWER_STARTUP_PAR2, TOWER_STARTUP_PAR3);
-		Packet_Put(TOWER_NUMBER_COMM, TOWER_NUMBER_PAR1, TowerNumber->s.Lo, TowerNumber->s.Hi);
-		Packet_Put(TOWER_VERSION_COMM, TOWER_VERSION_V, TOWER_VERSION_MAJ, TOWER_VERSION_MIN);
-		Packet_Put(TOWER_MODE_COMM, TOWER_MODE_PAR1, TowerMode->s.Lo, TowerMode->s.Hi);
-	}
+	__DI(); 		//Disable interrupts before peripheral module initialization
+	TowerInit();	//Initialize tower peripheral modules
+	__EI(); 		//Enable interrups
 
 	for (;;)
 	{
-		//Check if there is a packet in the retrieved data
-		if (Packet_Get())
+		if (Packet_Get())	//Check if there is a packet in the retrieved data
 		{
 			Packet_Handle();
 			LEDs_Toggle(LED_BLUE);
 		}
-
-		//Checks whether the RDRF or TDRE flags are set and retrieves/transmits data
-		UART_Poll();
+		UART_Poll();	//Checks whether the RDRF or TDRE flags are set and retrieves/transmits data
 	}
 
 	/*** Don't write any code pass this line, or it will be deleted during code generation. ***/
