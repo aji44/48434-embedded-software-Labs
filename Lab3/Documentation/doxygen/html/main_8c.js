b5dea5fb946be124488961a8ebe98c82da7e9701
corey-stidston <Corey.Stidston@student.uts.edu.au>
2017-05-02 21:48:40 +1000

    Removed old documentation

--- Lab3/Documentation/doxygen/html/main_8c.js
+++
@@ -1,9 +0,0 @@
-var main_8c =
-[
-    [ "FTM0Callback", "group__main__module.html#gad1796ae9fd33efded3fd853ba9e56d71", null ],
-    [ "main", "group__main__module.html#ga840291bc02cba5474a4cb46a9b9566fe", null ],
-    [ "PITCallback", "group__main__module.html#ga83e32d3437f8fdbf06974d23242a9462", null ],
-    [ "RTCCallback", "group__main__module.html#ga2dabe81b72bf8ec8155cd875975beb07", null ],
-    [ "TowerInit", "group__main__module.html#ga5a3442582792e61486089cf7ea3d83e9", null ],
-    [ "packetTimer", "group__main__module.html#gab6e49d19cb315cb4cf0412e4f6b901b3", null ]
-];
